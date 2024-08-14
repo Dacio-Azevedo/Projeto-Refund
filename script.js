523a8ff0f8de00cd5ee20d20c3911a456112c012
Dacio Azevedo <dacioherazan2@gmail.com>
2024-08-14 20:25:29 -0300

    Criando a informação da despesa

--- script.js
+++ script.js
@@ -56,12 +56,24 @@ function expenseAdd(newExpense) {
         const expenseIcon = document.createElement("img");
         expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
         expenseIcon.setAttribute("alt", "Ícone de tipo da despesa.");
+        
+        //Adicionando a info da despesa
+        const expenseInfo = document.createElement("div");
+        expenseInfo.classList.add("expense-info");
+        //Cria o nome da despesa
+        const expenseName = document.createElement("strong");
+        expenseName.textContent = newExpense.expense;
+        //Cria a categoria da despesa
+        const expenseCategory = document.createElement("span");
+        expenseCategory.textContent = newExpense.category_name;
+        //Adiciona nome e categoria na div das insformaçãoes da despesa
+        expenseInfo.append(expenseName,expenseCategory);
 
+        
         //Adiciona as informações no item
-        expenseItem.append(expenseIcon);
+        expenseItem.append(expenseIcon,expenseInfo);
         //Adiciona o item na lista
         expenseList.append(expenseItem);
-    
     } catch (error) {
         alert("Não foi possível atualizar a lista de despesas.")
         console.log(error);
