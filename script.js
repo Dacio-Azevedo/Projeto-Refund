3ad5d127e61ff85d92191e50da011f6f3badad28
Dacio Azevedo <dacioherazan2@gmail.com>
2024-08-14 20:44:17 -0300

    Criando o valor da despesa

--- script.js
+++ script.js
@@ -69,9 +69,15 @@ function expenseAdd(newExpense) {
         //Adiciona nome e categoria na div das insformaçãoes da despesa
         expenseInfo.append(expenseName,expenseCategory);
 
+        //Cria o valor da despesa
+        const expenseAmount = document.createElement("span");
+        expenseAmount.classList.add("expense-amount");
+        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$","")}`;
+
+
         
         //Adiciona as informações no item
-        expenseItem.append(expenseIcon,expenseInfo);
+        expenseItem.append(expenseIcon,expenseInfo, expenseAmount);
         //Adiciona o item na lista
         expenseList.append(expenseItem);
     } catch (error) {
